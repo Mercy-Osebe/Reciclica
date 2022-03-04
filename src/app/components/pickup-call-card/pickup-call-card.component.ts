2346dea16b4eb6652ca2a47a2ad266db5dcf83e2
Mercy Osebe <marcynyaata@gmail.com>
2022-03-04 11:47:17 +0300

    Adding Components Folder

--- src/app/components/pickup-call-card/pickup-call-card.component.ts
+++ src/app/components/pickup-call-card/pickup-call-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pickup-call-card',
@@ -6,6 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pickup-call-card.component.scss'],
 })
 export class PickupCallCardComponent implements OnInit {
+  @Input() hasHeader: boolean;
+  @Input() hasFooter: boolean;
+//The @Input help u to type the fields we want.
+  @Input() status: string;
+  @Input() updatedAt: string;
+  @Input() createdAt: string;
+  @Input() notes: string;
+
 
   constructor() { }
 
